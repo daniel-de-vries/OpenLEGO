2b0d31971c86425987d30e63b37d35e477dbe6bb
Daniël de Vries <danieldevries6@gmail.com>
2017-09-18 13:55:27 +0200

    Added wing_opt example to the examples/openlego folder, as well as an input.xml for and CMDOWS file for it.

---
+++ examples/openlego/wing_opt.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from openmdao.api import Problem, ScipyOptimizer
+from openlego.model import LEGOModel
+from openlego.recorders import NormalizedDesignVarPlotter, ConstraintsPlotter, SimpleObjectivePlotter
+
+
+if __name__ == '__main__':
+    # 1. Create a Problem
+    prob = Problem()
+    prob.set_solver_print(0)
+
+    # 2. Create the LEGOModel
+    model = prob.model = LEGOModel('wing_opt_MDG_MDF_GS.xml', '../kb/kb_wing_opt', '', 'wing_opt_output.xml')
+
+    # 3. Create a Driver object
+    driver = prob.driver = ScipyOptimizer()
+    driver.options['optimizer'] = 'SLSQP'
+    driver.options['disp'] = True
+    driver.options['tol'] = 1.0e-3
+    driver.opt_settings = {'disp': True, 'iprint': 2, 'ftol': 1.0e-3}
+
+    # 4. Setup the problem
+    prob.setup()
+    prob.run_model()
+    model.initialize_from_xml('wing_opt_input.xml')
+
+    # 6. Attach some Recorders
+    driver.add_recorder(NormalizedDesignVarPlotter())
+    driver.add_recorder(ConstraintsPlotter())
+    driver.add_recorder(SimpleObjectivePlotter())
+
+    # 5. Solve the problem
+    prob.run_driver()
+    prob.cleanup()

81b1e544d7aca74892800a631db149bffdcbee9e
Daniël de Vries <danieldevries6@gmail.com>
2017-10-23 09:46:13 +0200

    Added api module in the root, importing from the core module

---
+++ openlego/api.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+# Core
+from .core.xml_component import XMLComponent
+from .core.discipline_component import DisciplineComponent
+from .core.abstract_discipline import AbstractDiscipline
+from .core.model import LEGOModel

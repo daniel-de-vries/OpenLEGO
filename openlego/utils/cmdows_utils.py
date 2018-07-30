3e409603af53c3896cf11da3f9e4b3582d43bde9
imcovangent <I.vanGent@tudelft.nl>
2018-07-30 11:33:36 +0200

    Added function signature descriptions and docstrings

--- openlego/utils/cmdows_utils.py
+++ openlego/utils/cmdows_utils.py
@@ -6,10 +6,26 @@ from copy import copy
 from lxml.etree import _Element
 
 
-# TODO: add function signature description
-# TODO: add function docstring
 def get_related_parameter_uid(elem_param, full_xml):
+    # type: (_Element, _Element) -> (str, str)
+    """Function to retrieve the UID of the related parameter. This UID refers to the original local in the XML file of
+     the parameter and is used when a parameter is found in a CMDOWS file which is an architecture type or a higher
+     instance.
 
+    Parameters
+    ----------
+    elem_param : _Element
+        Element of the parameter to be mapped.
+    full_xml : _Element
+        Element of the full XML file in which the related parameter UID should be found.
+
+    Returns
+    -------
+    param : str
+        Original parameter UID for which the related UID is searched for.
+    mapped : str
+        The found related parameter UID.
+    """
     param = elem_param.attrib['uID']
     if isinstance(elem_param.find('relatedParameterUID'), _Element):
         mapped = elem_param.find('relatedParameterUID').text
@@ -22,17 +38,21 @@ def get_related_parameter_uid(elem_param, full_xml):
 
 
 def get_element_by_uid(xml, uid):
-    # type: (etree._ElementTree, str) -> etree._ElementTree
+    # type: (_Element, str) -> _Element
     """Method to get the element based on a UID value.
 
     Parameters
     ----------
-        xml : :obj:`etree._ElementTree`
-            `etree._ElementTree` of an XML file where the uID should be found.
+        xml : _Element
+            Element of an XML file where the uID should be found.
 
         uid : str
             uID to be found.
 
+    Returns
+    -------
+    _Element
+        Element with the right UID.
     """
     xpath_expression = get_uid_search_xpath(uid)
     els = xml.xpath(xpath_expression)
@@ -44,17 +64,19 @@ def get_element_by_uid(xml, uid):
     return els[0]
 
 
-# TODO: add function signature description
-# TODO: use correct docstring format
 def get_uid_search_xpath(uid):
-    """ Method to get the XPath expression for a uID which might contain quote characters.
+    # type: (str) -> str
+    """Method to get the XPath expression for a UID that might contain quote characters.
 
-    # TODO: update docstring
+    Parameters
+    ----------
+    uid : str
+        Original UID string with XPath expression.
 
-    :param uid: uID
-    :type uid: str
-    :return: XPath expression
-    :rtype: str
+    Returns
+    -------
+    str
+        Processed XPath expression to escape quote characters using "concat()".
     """
     if '"' in uid or '&quot;' in uid:
         uid_concat = "concat('%s')" % uid.replace('&quot;', "\',\'\"\',\'").replace('"', "\',\'\"\',\'")
@@ -63,32 +85,36 @@ def get_uid_search_xpath(uid):
         return './/*[@uID="' + uid + '"]'
 
 
-# TODO: add function signature description
-# TODO: use correct docstring format
-def get_loop_nesting_dict(r):
-    """Function to make a dictionary of the loop hierarchy based on the loopNesting element in a CMDOWS file.
+def get_loop_nesting_obj(elem):
+    # type: (_Element) -> Optional[list]
+    """Function to make an object of the loop hierarchy based on the loopNesting element in a CMDOWS file.
 
-    :param r:
-    :type r:
-    :return:
-    :rtype:
+    Parameters
+    ----------
+    elem : _Element
+        Element in the XML file (loopNesting or one of its subelements)
+
+    Returns
+    -------
+    list
+        A list object containing dictionaries and string entries to represent the loopNesting element.
     """
     basic_list = []
     basic_dict = {}
-    d = copy(r.attrib)
-    if r.tag == 'loopNesting' or r.tag == 'loopElements' or r.tag == 'functionElements':
-        for x in r.iterchildren():
-            basic_list.append(get_loop_nesting_dict(x))
+    d = copy(elem.attrib)
+    if elem.tag == 'loopNesting' or elem.tag == 'loopElements' or elem.tag == 'functionElements':
+        for x in elem.iterchildren():
+            basic_list.append(get_loop_nesting_obj(x))
         if len(basic_list) == 1 and isinstance(basic_list[0], list):
             return basic_list[0]
         else:
             return basic_list
-    elif r.tag == 'loopElement':
+    elif elem.tag == 'loopElement':
         basic_dict[d['relatedUID']] = []
-        for x in r.iterchildren():
-            basic_dict[d['relatedUID']].extend(get_loop_nesting_dict(x))
+        for x in elem.iterchildren():
+            basic_dict[d['relatedUID']].extend(get_loop_nesting_obj(x))
         return basic_dict
-    elif r.tag == 'functionElement':
-        return r.text
+    elif elem.tag == 'functionElement':
+        return elem.text
     else:
         raise AssertionError('Something went wrong in this function...')

3220bdeb1d9f90fc535590d2da5e14813ac6ada9
Daniël de Vries <danieldevries6@gmail.com>
2018-08-01 12:43:26 -0500

    Added/Updated copyright and license statements where necessary.

--- openlego/core/discipline_component.py
+++ openlego/core/discipline_component.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright 2017 D. de Vries
+Copyright 2018 D. de Vries
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

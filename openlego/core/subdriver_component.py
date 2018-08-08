d6cb7a49b3b7c5954670d28aef78ed87e49254e8
imcovangent <I.vanGent@tudelft.nl>
2018-08-08 17:05:00 +0200

    Created skeleton for subdriver_component.py

---
+++ openlego/core/subdriver_component.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright 2018 I. van Gent and D. de Vries
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This file contains the definition the `SubDriverComponent` class.
+"""
+
+from openmdao.api import ExplicitComponent
+
+class SubDriverComponent(ExplicitComponent):
+    """Abstract base class exposing an interface to use subdriver (or nested) drivers within an OpenMDAO model. This
+    nested subdriver appears as an ExplicitComponent in the top-level hierarchy, but configures and executes its own
+    LEGOProblem() and LEGOModel() instances inside.
+
+    Attributes
+    ----------
+    TODO: Add attributes and complete class description
+    """
+    def initialize(self):
+        self.options.declare('driver_id')
+        self.options.declare('cmdows_elem')  # TODO: or CMDOWS object?
+        self.options.declare('show_model')
+
+    def setup(self):
+
+        # Add inputs
+        #self.add_input()
+
+        # Add outputs
+        # self.add_output()
+
+        # Declare partials
+        # self.declare_partials('*', '*')
+
+        # Set subproblem
+        #self.prob = LEGOProblem(other inputs?, subdriver_id = self.options['driver_id'])
+
+        # Add missing connections (?)
+
+        # Setup
+        #self.prob.setup()
+        #self.prob.final_setup()
+
+        # View model?
+        #if self.options['show_model']:
+        pass
+
+    def compute(self, inputs, outputs):
+        #p = self.prob
+
+        # Push global inputs down
+        # p['x'] = inputs['x']
+        # etc.
+
+        # Run the driver
+        # p.run_driver()
+
+        # Pull the value back up to the output array
+        # outputs['y'] = p['y']
+        pass
